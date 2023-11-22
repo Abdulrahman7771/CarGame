06d0e3183f655ed06d4c68d8f3d0589d78c1e945
unknown <ana.alandalib777@gmail.com>
2023-11-22 09:28:32 +0200

    FinalVersion

--- CarGame/CarGame.py
+++ CarGame/CarGame.py
@@ -1,23 +1,26 @@
 import pygame
-from pygame.locals import *
+import pygame_widgets
+from pygame_widgets.button import Button
 from random import randint
 import time
+import os
 from os.path import exists
 
-sprites = ["enemy1.png","enemy2.png","enemy3.png","enemy3.png"]
+sourceFileDir = os.path.dirname(os.path.abspath(__file__))
+
+sprites = ["enemy1.png","enemy2.png","enemy3.png","enemy4.png"]
 
 class sprite(pygame.sprite.Sprite):
     def __init__(self,img,pos,screen,layer):
+        path = os.path.join(sourceFileDir , "assets", img)
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(img).convert_alpha()
+        self.image = pygame.image.load(path).convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.pos = pos
-        pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
+        #pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
         screen.blit(self.image,self.rect)
         self._layer = layer
-        pygame.sprite.Sprite.__init__(self)
-
 
 class car(sprite):
     def __init__(self,img,pos,screen,layer):
@@ -26,12 +29,12 @@ class car(sprite):
         self.CollisionRect.center = pygame.Vector2(pos.x+15,pos.y+15)
         self.CollisionRect.width -= 30
         self.CollisionRect.height -= 40
-        pygame.draw.rect(self.image,"green",[15,15,self.CollisionRect.width,self.CollisionRect.height],5)
+        #pygame.draw.rect(self.image,"green",[15,15,self.CollisionRect.width,self.CollisionRect.height],5)
     def update(self,pos):
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.CollisionRect.center = pygame.Vector2(pos.x+15,pos.y+15)
-        pygame.draw.rect(self.image,"green",[15,15,self.CollisionRect.width,self.CollisionRect.height],5)
+        #pygame.draw.rect(self.image,"green",[15,15,self.CollisionRect.width,self.CollisionRect.height],5)
         #pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
         screen.blit(self.image,self.rect)
         
@@ -46,62 +49,86 @@ class Enemy(sprite):
         if(not lose):
             self.pos.y += (self.speed)*dt
         self.rect.center = self.pos
-        pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
+        #pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
         screen.blit(self.image,self.rect)
         self.reset()
     def reset(self):
         if(self.rect.y>=screen.get_height()):
             pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.image.load(sprites[randint(0,3)]).convert_alpha()
+            path = os.path.join(sourceFileDir , "assets", sprites[randint(0,3)])
+            self.image = pygame.image.load(path).convert_alpha()
             self.rect = self.image.get_rect()
-            self.pos = pygame.Vector2(randint(int(x+50),int(lx)),-400)
+            self.pos = pygame.Vector2(randint(int(x+50),int(lx)),-300)
             self.rect.center = self.pos
-            pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
+            #pygame.draw.rect(self.image,"red",[0,0,self.rect.width,self.rect.height],5)
             screen.blit(self.image,self.rect)
             self.speed = randint(int(1),int(2))
-
+    def GetSpeed(self):
+        return self.speed
+    def SetSpeed(self,s):
+        self.speed = s
+    def SetPos(self,p):
+        self.pos.x +=p
+        
 if(not(exists("score.txt"))):
             with open("score.txt", "w") as f:
                 f.write("0")
             f.close()
+            
 pygame.init()
 
 running = True
 lost=False
-clock = pygame.time.Clock()
+CanModifyScore = True
+
 dt = 0
-StartTime = time.time()
 score = 0
 HighScore = 0
+
+performance = "Good game"
+
+clock = pygame.time.Clock()
+StartTime = time.time()
 screen = pygame.display.set_mode()  
-road = pygame.image.load("road.png").convert()
+roadpath = os.path.join(sourceFileDir , "assets", "road.png")
+road = pygame.image.load(roadpath).convert()
 pygame.draw.rect(road,"red",[0,0,road.get_rect().width,road.get_rect().height],5)
 screen.blit(road,((screen.get_width() / 2 )-(road.get_width()/2), -10))
 myfont = pygame.font.SysFont("monospace", 24)
 
+def end():
+    global running
+    running = False
+
+
 x = ((int(screen.get_width() / 2 ))-(int(road.get_width()/2))) + 100
 lx = (x + road.get_width()) -300
-group = pygame.sprite.LayeredUpdates()
+
 PPos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 1.2)
 player = car("player7.png",PPos,screen,1)
-enemy0 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,100)),screen,group,0,2)
-enemy1 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,100)),screen,group,0,3)
-enemy2 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,100)),screen,group,0,1)
-enemy3 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,100)),screen,group,0,2)
-enemy4 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,100)),screen,group,0,2)
-CanModifyScore = True
-performance = "Good game"
+
+group = pygame.sprite.LayeredUpdates()
+enemy0 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,-100)),screen,group,0,2)
+enemy1 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,-100)),screen,group,0,3)
+enemy2 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,-100)),screen,group,0,1)
+enemy3 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,-100)),screen,group,0,2)
+enemy4 = Enemy(sprites[randint(0,3)],pygame.Vector2(randint(int(x+50),int(lx)),randint(-400,-100)),screen,group,0,2)
+
 
 while running:
     screen.fill("green")
     
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 if(lost):
                     running = False
+            if event.key == pygame.K_r:
+                if(lost):
+                    pass
 
     screen.blit(road,((screen.get_width() / 2 )-(road.get_width()/2), -10))
     
@@ -109,12 +136,10 @@ while running:
     group.update(lost)
     group.draw(screen)
     
-    #coll = pygame.sprite.spritecollideany(player, group, collided = None)
     coll = player.CollisionRect.collidelist(group.get_sprites_from_layer(0))
-    print(coll)
     if(coll != -1):
         lost = True
-        
+
     if(not lost):
         keys = pygame.key.get_pressed()     
         if keys[pygame.K_a]:
@@ -123,12 +148,35 @@ while running:
         if keys[pygame.K_d]:
             if(not((player.rect.x + (7 * dt))>=lx)):
                 PPos.x += 7 * dt
+        enemies = group.sprites()
+        for i, e1 in enumerate(enemies):
+            for e2 in enemies[i+1:]:
+                if pygame.sprite.collide_mask(e1, e2):
+                    if(e1.rect.y<=e2.rect.y):
+                        orspeed = e1.GetSpeed()
+                        e1.SetSpeed(1)
+                        if(e1.rect.x<=e2.rect.x):
+                            if((e1.rect.x - 1 )>=x):
+                                e1.SetPos(-1)
+                        else:
+                            if((e1.rect.x + 1 )<=lx):
+                                e1.SetPos(1)   
+                        e1.SetSpeed(orspeed)
+                    else:
+                        orspeed = e2.GetSpeed()
+                        e2.SetSpeed(1)
+                        if(e2.rect.x<=e1.rect.x):
+                            if((e2.rect.x - 1 )>=x):
+                                e2.SetPos(-1)
+                        else:
+                            if((e2.rect.x + 1 )<=lx):
+                                e2.SetPos(1)   
+                        e2.SetSpeed(orspeed)
         score = int((time.time() - StartTime))
     else:
         if(CanModifyScore):
             with open("score.txt", "r") as f:
                 HighScore = f.read()
-                print(f"{f.read()}: ssssssssssssss")
                 f.close()
             with open("score.txt", "w") as f:
                 if(int(HighScore)<score):
@@ -145,14 +193,25 @@ while running:
         EndGameText = myfont.render(f"{performance} Score: {score}", 0, "red")
         HighText = myfont.render(f"HighScore: {HighScore}", 0, "red")
         congrats = myfont.render(f"Better luck next time", 0, "red")
-        exit = myfont.render(f"press q to exit", 0, "red")
+        button = Button(
+            screen, (screen.get_width() / 2),
+            (screen.get_height()/2)
+            , 200, 50, text='Quit',
+            fontSize=24, margin=10,
+            inactiveColour=(255, 0, 0),
+            pressedColour=(0, 255, 0), radius=20,
+            onClick= lambda: end()
+        )
+        RecPivot = ((EndGameText.get_height()+HighText.get_height()+congrats.get_height()+button.getHeight())/2)
+        button.setX(button.getX()-(button.getWidth()/2))
+        button.setY(button.getY()-(button.getHeight())+RecPivot)
         pygame.draw.rect(screen,"#00FFB9"
-                        ,[(screen.get_width() / 2)-(congrats.get_width()/2), screen.get_height()/2
-                        ,congrats.get_width(),EndGameText.get_height()*4],0)
-        screen.blit(EndGameText, ((screen.get_width() / 2)-(EndGameText.get_width()/2), screen.get_height()/2))
-        screen.blit(HighText, ((screen.get_width() / 2)-(HighText.get_width()/2), (screen.get_height()/2)+EndGameText.get_height()))
-        screen.blit(congrats, ((screen.get_width() / 2)-(congrats.get_width()/2), (screen.get_height()/2)+EndGameText.get_height()+HighText.get_height()))
-        screen.blit(exit, ((screen.get_width() / 2)-(exit.get_width()/2), (screen.get_height()/2)+EndGameText.get_height()+congrats.get_height()+HighText.get_height()))
+                        ,[(screen.get_width() / 2)-(congrats.get_width()/2), (screen.get_height()/2) - RecPivot
+                        ,congrats.get_width(),EndGameText.get_height()*3+button.getHeight()],0)
+        screen.blit(EndGameText, ((screen.get_width() / 2)-(EndGameText.get_width()/2), (screen.get_height()/2)-RecPivot))
+        screen.blit(HighText, ((screen.get_width() / 2)-(HighText.get_width()/2), (screen.get_height()/2)-RecPivot+EndGameText.get_height()))
+        screen.blit(congrats, ((screen.get_width() / 2)-(congrats.get_width()/2), (screen.get_height()/2)-RecPivot+EndGameText.get_height()+HighText.get_height()))
+        pygame_widgets.update(events)
 
     scoretext = myfont.render(f"Score: {score}", 0, "red")
     pygame.draw.rect(screen,"#00FFB9",[50,25,scoretext.get_width(),scoretext.get_height()],0)
@@ -163,44 +222,3 @@ while running:
     dt = clock.tick(30) / 10
     
 pygame.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#players = pygame.sprite.Group()
-#players.add(player)
-#print(players)
-#100*191
-#players.update()
-#players.draw(screen)
-#screen.blit(grass,(0,0))
-#grass = pygame.image.load("Grass.png")
-#player.update(PPos)
-#screen.blit(grass,(0,0))
